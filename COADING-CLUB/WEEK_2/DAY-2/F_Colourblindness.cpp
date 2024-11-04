30446df2844353497c5f03378246e0dff7f0b2ab
Shammo Saha <168102614+Shammo1@users.noreply.github.com>
2024-11-04 18:28:15 +0600

    Add files via upload

---
+++ COADING-CLUB/WEEK_2/DAY-2/F_Colourblindness.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        string s, k;
+        cin >> s >> k;
+
+        bool flag = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] != k[i])
+            {
+
+                if (s[i] == 'G' && k[i] == 'B' || s[i] == 'B' && k[i] == 'G')
+                {
+                                }
+                else
+                {
+                    flag = true;
+                }
+            }
+        }
+        if (flag == false)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
