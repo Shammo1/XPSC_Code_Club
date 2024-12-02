80807c22f0547b5e5bde939ab067b1a765c60523
Shammo Saha <168102614+Shammo1@users.noreply.github.com>
2024-12-02 21:32:03 +0600

    Add files via upload

---
+++ COADING-CLUB/WEEK-6/DAY-1/A_Phone_Desktop.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        int ans = (y + 1) / 2;
+        x -= (ans * 5 * 3 - y * 2 * 2);
+        x = max(x, 0);
+
+        ans += (x + 5 * 3 - 1) / (5 * 3);
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
