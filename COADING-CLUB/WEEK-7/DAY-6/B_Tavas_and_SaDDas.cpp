54f2bea7be71b109a7e7f714ea42d37a4293994b
Shammo Saha <168102614+Shammo1@users.noreply.github.com>
2024-12-14 12:10:37 +0600

    Add files via upload

---
+++ COADING-CLUB/WEEK-7/DAY-6/B_Tavas_and_SaDDas.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int len = 0;
+    int ans = 0;
+    while (n)
+    {
+        if (n % 10 == 7)
+            ans += (1 << len);
+        n /= 10;
+        len++;
+    }
+    for (int i = 1; i < len; i++)
+        ans += (1 << i);
+    cout << ans + 1 << endl;
+    return 0;
+}
